285416c6d434e4afbc92cd08b73825d99573dd6e
JatinVira <jatinvira666@gmail.com>
2022-11-02 12:45:34 +0530

    added a service and client execution in python.

---
+++ day_05/scripts/add_two_ints_server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import rospy
+from day_05.srv import AddTwoInts, AddTwoIntsResponse
+
+def handler(req):
+    print("Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b)))
+    return AddTwoIntsResponse(req.a + req.b)
+
+def add_two_ints_server():
+    rospy.init_node("Server", anonymous= False)
+    s = rospy.Service("add_two_ints", AddTwoInts, handler)
+    rospy.loginfo("Ready to Serve!")
+    rospy.spin()
+
+if __name__ == '__main__':
+    add_two_ints_server()
